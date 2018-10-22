1a26e8b12e55b34105ebe5b87877326f104c4294
Maksym Stepanenko <maksym.stepanenko@opower.com>
2018-10-22 17:50:44 +0300

    tcpdump added

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -5,6 +5,9 @@ echo "********************************************************"
 while ! `nc -z eureka $EUREKASERVER_PORT `; do sleep 3; done
 echo ">>>>>>>>>>>> Eureka has started"
 
+nohup tcpdump -A -s 0 'tcp port 8888 and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)' \
+-w /usr/local/config-service/pcap/config.pcap &
+
 echo "********************************************************"
 echo "Starting config-service "
 echo "********************************************************"
